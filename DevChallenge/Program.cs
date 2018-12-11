86a3793f4de582e04545f2fbd36ae665afe4bf90
Liudvikas Taluntis <liudvikast@gmail.com>
2018-12-11 17:25:55 +0000

    Done It.

---
+++ DevChallenge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DevChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for(int number=1; number<101; number++)
+            {
+                string printable = "{0} ";
+
+                if (number % 3 == 0) { printable += "Fizz "; }
+                if (number % 5 == 0) { printable += "Buzz "; }
+
+                printable = printable.Remove(printable.Length - 1);
+
+                Console.WriteLine(printable, number);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
